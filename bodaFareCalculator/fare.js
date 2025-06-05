baec20e4a0dfc84f30e4f22a7a86553c46c9548e
Ralph <njugunaralph@gmail.com>
2025-06-06 00:24:54 +0300

    boda fare confirmed

--- bodaFareCalculator/fare.js
+++ bodaFareCalculator/fare.js
@@ -4,5 +4,21 @@ const readline = require('readline').createInterface({
 });
 
 function calculateBodaFare(distanceInKm){
-    
+    const baseFare = 50;
+    const chargePerKm = 15;
+    const totalFare = baseFare + (distanceInKm * chargePerKm);
+
+    return [
+        `Uko kwote? Io ni ${distanceInKm} km:`,
+        `Ukikalia Pikipiki: KES ${baseFare}`,
+        `Mpaka Huko: KES ${distanceInKm * chargePerKm}`,
+        `Total: KES ${totalFare}`,
+        `Panda Pikipiki!`
+    ];
 }
+
+readline.question('Unafika wapi Mkubwa? Kilometer ngapi?' ,(input) => {
+    const kms = parseInt(input, 10);
+    console.log(calculateBodaFare(kms).join('\n'));
+    readline.close();
+});
